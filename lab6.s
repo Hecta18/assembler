e473feaf23a60decc40c61626ea5cdd41c9d2fa4
Hecta18 <62799494+Hecta18@users.noreply.github.com>
2025-05-14 19:54:43 -0600

    substractions & other opcodes

--- lab6.s
+++ lab6.s
@@ -1,6 +1,18 @@
 .global _start
 _start:
-	
+	//carga y resta
 	mov r1, #0xC0
 	mov r2, #0x1F
 	sub r3, r1, r2
+
+	//tipos de resta
+	RSB r0, r1, r2
+	SUB r0, r2, r1
+
+	//resultado 0
+	mov r7, #0xA
+	mov r4, #0x0
+	MUL r7, r1, r4
+	cpy r7, r4
+	rev r7, r4
+	add r7, r4
